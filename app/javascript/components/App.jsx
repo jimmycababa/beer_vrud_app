9ce0ed3f9adf757f19c186e3756da86ee9c5be56
jimmycababa <jimmycababa@gmail.com>
2021-08-06 21:02:17 -0500

    changed one class to functional

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,4 +2,7 @@ import React from "react";
 import Routes from "../routes/index";
 import "antd/dist/antd.css";
 
-export default () => <>{Routes}</>;
+export default () => <>{<Routes />}</>;
+
+// export default () => {return (<div>HELLOW WORLD</div>)}
+// this needs a return statement
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 
-export default (
-  <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
-  </Router>
-);
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default RouterComponent;
 
 // each route must be mapped within a different Route tag. 
 // the path parameter must match the URI of each route endpoint
