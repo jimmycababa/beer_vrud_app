69a9464cd81a4284e17fffc382b5e4904cf7c51f
jimmycababa <jimmycababa@gmail.com>
2021-08-13 22:46:43 -0500

    added column to beers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_201020) do
+ActiveRecord::Schema.define(version: 2021_08_14_034407) do
 
   create_table "beers", force: :cascade do |t|
     t.string "brand"
     t.string "style"
     t.string "country"
     t.integer "quantity"
-    t.datetime "created_at", precision: 6, null: true
+    t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "size"
   end
 
 end
